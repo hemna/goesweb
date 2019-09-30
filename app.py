6ebfccd84ba260d549811f565743aa80b86af240
Walter A. Boring IV <waboring@hemna.com>
2019-09-30 17:14:23 -0400

    mep

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +12,4 @@ def main():
     return "ASS"
 
 if __name__ == '__main__':
-    app.run(port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
